add4c1af90d06be8dcf519ebf8b65857b31855d3
avelirs <sibilevanton6@gmail.com>
2024-08-27 15:44:07 +0300

    init: change a header style

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -8,25 +8,24 @@ function Header() {
       <div className="flex items-center">
         <img src="/api/placeholder/50/50" alt="SMK Law Logo" className="mr-2" />
         <h1 className="text-xl font-bold">SMK Law</h1>
-        <p className="ml-4 text-sm">Артур Сімейко адвокат</p>
+        <p className="ml-4 text-sm">Максим Сибилев адвокат</p>
       </div>
       <nav>
         <ul className="flex space-x-4">
-          <li >
-            <Link to="/">Головна</Link>
+          <li>
+            <Link to="/" className="text-lg font-semibold text-gray-700 hover:text-blue-600 transition-colors duration-300">Головна</Link>
           </li>
           <li>
-            <Link to="/position">Позиція</Link  >
+            <Link to="/position" className="text-lg font-semibold text-gray-700 hover:text-blue-600 transition-colors duration-300">Позиція</Link>
           </li>
           <li>
-            <Link to="/team">Команда</Link>
+            <Link to="/team" className="text-lg font-semibold text-gray-700 hover:text-blue-600 transition-colors duration-300">Команда</Link>
           </li>
           <li>
-            <Link to="/services">Послуги</Link>
+            <Link to="/services" className="text-lg font-semibold text-gray-700 hover:text-blue-600 transition-colors duration-300">Послуги</Link>
           </li>
-       
           <li>
-            <Link to="/contacts">Контакти</Link>
+            <Link to="/contacts" className="text-lg font-semibold text-gray-700 hover:text-blue-600 transition-colors duration-300">Контакти</Link>
           </li>
         </ul>
       </nav>
