a2d7c945d10931f6263b4c49e94289e69320a6c2
avelirs <sibilevanton6@gmail.com>
2024-08-28 16:27:26 +0300

    init: add icons to foooter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import SendIcon from "@mui/icons-material/Send";
+import FacebookIcon from "@mui/icons-material/Facebook";
 
 function Footer() {
   const [email, setEmail] = useState("");
@@ -28,13 +31,17 @@ function Footer() {
     <footer className='bg-gray-900 text-white py-12 flex l items-center'>
       <div>
         <p>Напишіть свою електронну пошту і ми зв’яжемось з вами</p>
-        <form onSubmit={handleSubmit} action=''>
+        <form
+          onSubmit={handleSubmit}
+          className='flex items-center mb-4 md:mb-0'
+        >
           <input
             type='email'
+            placeholder='Email'
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            placeholder='Email'
-            className='bg-gray-800 text-white px-4 py-2 rounded-md'
+            className='bg-white text-gray-900 px-3 py-2 rounded-l-md focus:outline-none'
+            required
           />
           <button
             type='submit'
@@ -44,7 +51,33 @@ function Footer() {
           </button>
         </form>
       </div>
-      <div className='flex space-x-4'></div>
+      <div className='flex space-x-4'>
+        {" "}
+        <a
+          href='https://instagram.com'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='hover:text-gray-300'
+        >
+          <InstagramIcon style={{ fontSize: 24 }} />
+        </a>
+        <a
+          href='https://telegram.org'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='hover:text-gray-300'
+        >
+          <SendIcon style={{ fontSize: 24 }} />
+        </a>
+        <a
+          href='https://facebook.com'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='hover:text-gray-300'
+        >
+          <FacebookIcon style={{ fontSize: 24 }} />
+        </a>
+      </div>
     </footer>
   );
 }
