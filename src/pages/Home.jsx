bef6e46b2d1c5ae0aa48fc23d57489149097832c
avelirs <sibilevanton6@gmail.com>
2024-08-28 13:20:18 +0300

    init: somechange

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
-
 function Home() {
-
-    return (
-        <div>
-            <h1>Головна</h1>
-        </div>
-    )
+  return (
+    <div className='container'>
+      <h1 className='text-4xl font-bold'>Legal Expertise You Can Trust</h1>
+      <p className='text-lg'>Your reliable partner in legal matters.</p>
+      <a href='#contact' className='cta-button'>
+        Contact Us
+      </a>
+    </div>
+  );
 }
 
 export default Home;
